28b75477ebe3c9d9fb50bc9d90c2d4ec761c788b
zeeun <98653814+zeeun@users.noreply.github.com>
2023-01-16 19:53:47 +0900

    Add files via upload

---
+++ src/views/ScheduleView.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div>
+        <full-calendar :events="fcEvents" lang="en"></full-calendar>
+        
+    </div>
+</template>
+<script>
+var demoEvents = [
+    {
+        title: 'Sunny Out of Office',
+        start: '2023-01-25',
+        end: '2023-01-28'
+    }
+]
+export default {
+    data() {
+        return {
+            fcEvents: demoEvents
+        }
+    },
+    components: {
+        'full-calendar': require('vue-fullcalendar')
+    }
+}
+
+</script>
+<style lang="">
+    
+</style>
