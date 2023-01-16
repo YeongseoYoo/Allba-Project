505b5f1623756e9d4b69494435f65cf191dbad4a
zeeun <wwwqtr@gmail.com>
2023-01-16 19:44:22 +0900

    [CREATE]선택방 ㅈ

---
+++ allba_front/src/views/RegisterView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <h2 class="title">지점등록</h2>
+    <hr class="line">
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.title{
+    font-weight:600;
+    font-size:32px;
+    margin-top: 48px;
+    margin-left:8%;
+}
+
+.line{
+    width:90%;
+    margin-left:5%
+} 
+</style>
--- allba_front/src/views/ScheduleView.vue
+++ allba_front/src/views/ScheduleView.vue
@@ -1,5 +1,61 @@
 <template lang="">
     <div>
+    <h2 class="title">스케쥴링</h2>
+    <hr class="line">
+
+   <div class="p-4 mt-5" >
+        <div class="row g-1">
+   
+            <div class="row">
+                <div class="col-sm-4 col-lg-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title" style="">눈송행복점</h5>
+                            <p class="card-text" >12/13</p>
+                            <a href="#" class="btn btn-light">참가요청</a>
+    
+                        </div>
+                    </div>
+                </div>           
+          
+
+                 <div class="col-sm-4 col-lg-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">눈송행복점</h5>
+                            <p class="card-text; my-1">12/13</p>
+                            <a href="#" class="btn btn-light">참가요청</a>
+            
+                        </div>
+                    </div>
+                </div>
+   
+
+                <div class="col-sm-4 col-lg-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">눈송행복점</h5>
+                            <p class="card-text; my-1">12/13</p>
+                            <a href="#" class="btn btn-light">참가요청</a>
+                        </div>
+                    </div>
+                </div>
+
+                                <div class="col-sm-4 col-lg-3">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h5 class="card-title">눈송행복점</h5>
+                                            <p class="card-text; my-1">12/13</p>
+                                            <a href="#" class="btn btn-light">참가요청</a>
+                                        </div>
+                                    </div>
+                                </div>
+           
+
+        </div>
+            </div>
+            </div>
+         <hr class="underline">
         <full-calendar :events="fcEvents" lang="en"></full-calendar>
         
     </div>
@@ -24,6 +80,23 @@ export default {
 }
 
 </script>
-<style lang="">
-    
+<style scoped>
+.title {
+    font-weight: 600;
+    font-size: 32px;
+    margin-top: 48px;
+    margin-left: 8%;
+}
+
+.line {
+    width: 90%;
+    margin-left: 5%
+}
+.unedrline{
+    width:98%;
+    margin-left:1%;
+    margin-top:2%;
+
+    margin-bottom:10%;
+}
 </style>
