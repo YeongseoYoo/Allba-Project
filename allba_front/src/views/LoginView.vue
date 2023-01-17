64a6819fc571652f2d3cd82ea529f1afa24c4d02
zeeun <wwwqtr@gmail.com>
2023-01-17 11:33:50 +0900

    Revert "[CREATE]지점등록 제작, 로그인화면수정"
    This reverts commit 1cadf84226f5b54e369a4fee5ea8c12fb571c058.

--- allba_front/src/views/LoginView.vue
+++ allba_front/src/views/LoginView.vue
@@ -1,6 +1,16 @@
 <template>
+<<<<<<< HEAD
     <div>
         login
+=======
+    <div class="main">
+        <div class="container">
+            <h1 class="logo">Login</h1>
+            <input type="text" placeholder="ID" id="id" class="account">
+            <input type="password" placeholder="Password" id="password" class="account">
+            <button id="login" class="account">login</button>
+        </div>
+>>>>>>> parent of 1cadf842 ([CREATE]지점등록 제작, 로그인화면수정)
     </div>
 </template>
 <script>
@@ -8,6 +18,53 @@ export default {
     
 }
 </script>
+<<<<<<< HEAD
 <style>
     
+=======
+
+<style scoped>
+.main {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border-bottom: 1px solid lightgrey;
+}
+.container{
+    margin-left:35%;
+    width: 50%;
+    margin-bottom:20%;
+}
+.logo {
+
+    margin-top:18%;
+    margin-bottom:20px;
+    font-size:24px;
+    justify-content: center;
+}
+
+
+#login {
+    width: 50%;
+    left:25%;
+    background-color: rgb(182, 165, 94);
+    border-color: transparent;
+    color: white;
+}
+
+.account {
+    width:50%;
+    display: block;
+    margin-bottom: 3px;
+    padding: 3px;
+    border: 1px solid lightgray;
+    border-radius: 3px;
+}
+
+#alert {
+    border-color: transparent;
+}
+>>>>>>> parent of 1cadf842 ([CREATE]지점등록 제작, 로그인화면수정)
 </style>
